7a799bca7f46dd5bfc4420dc1e7a11cba4aaaec3
kamosoft <thomas.bruyelle@gmail.com>
2011-03-05 18:58:24 +0100

    userPhotos

--- src/com/kamosoft/flickrwidget/FlickrConnectActivity.java
+++ src/com/kamosoft/flickrwidget/FlickrConnectActivity.java
@@ -2,11 +2,17 @@ package com.kamosoft.flickrwidget;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-import com.zmosoft.flickrfree.AuthenticateActivity;
+import com.kamosoft.flickr.APICalls;
+import com.kamosoft.flickr.AuthenticateActivity;
+import com.kamosoft.flickr.RestClient;
+import com.kamosoft.flickr.model.JsonFlickrApi;
 
 public class FlickrConnectActivity
     extends Activity
@@ -21,8 +27,38 @@ public class FlickrConnectActivity
         super.onCreate( savedInstanceState );
         setContentView( R.layout.connect );
 
-        AuthenticateActivity.registerAppParameters( this, getString( R.string.api_key ),
-                                                    getString( R.string.api_secret ), getString( R.string.auth_url ) );
+        SharedPreferences prefs = getSharedPreferences( "Auth", MODE_APPEND );
+        String userId = prefs.getString( "nsid", null );
+
+        Button connectButton = (Button) findViewById( R.id.connect_button );
+
+        if ( userId == null ) //FIXME find another way to check is auth is OK
+        {
+            AuthenticateActivity
+                .registerAppParameters( this, getString( R.string.api_key ), getString( R.string.api_secret ),
+                                        getString( R.string.auth_url ) );
+            connectButton.setEnabled( true );
+        }
+        else
+        {
+            RestClient.setAuth( this );
+            connectButton.setEnabled( false );
+            showActivityUserPhotos( userId );
+        }
+    }
+
+    private void showActivityUserPhotos( String userId )
+    {
+        try
+        {
+            JsonFlickrApi jsApi = APICalls.getActivityUserPhotos( userId, "5d", "", "" );
+            Log.i( "FlickrWidget", jsApi.toString() );
+        }
+        catch ( Exception e )
+        {
+            Log.e( "FlickrWidget", e.getMessage() );
+        }
+
     }
 
     public void connect( View view )
@@ -40,6 +76,8 @@ public class FlickrConnectActivity
                 if ( resultCode == AuthenticateActivity.AUTH_SUCCESS )
                 {
                     Toast.makeText( this, R.string.connectOK, Toast.LENGTH_SHORT ).show();
+                    String userId = getSharedPreferences( "Auth", MODE_APPEND ).getString( "nsid", null );
+                    showActivityUserPhotos( userId );
                 }
                 else
                 {
