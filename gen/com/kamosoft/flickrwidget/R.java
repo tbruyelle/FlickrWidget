11451255e5da02eb557b4765e4bfe7a2086489b5
tbruyelle <thomas.bruyelle@gmail.com>
2011-03-18 16:23:53 +0100

    min sdk version 7 (2.1) because of RemoteViews.addView, removeAllViews...

--- gen/com/kamosoft/flickrwidget/R.java
+++ gen/com/kamosoft/flickrwidget/R.java
@@ -20,31 +20,31 @@ public final class R {
         public static final int widget_bg=0x7f020006;
     }
     public static final class id {
-        public static final int AuthHelpInfo=0x7f070001;
-        public static final int AuthInputLayout=0x7f070007;
-        public static final int AuthLayout=0x7f070003;
-        public static final int AuthWeb=0x7f070004;
-        public static final int BtnOK=0x7f070002;
-        public static final int LblAuthCode=0x7f070006;
-        public static final int TokenInputLayout=0x7f070005;
-        public static final int authnum1=0x7f070008;
-        public static final int authnum2=0x7f070009;
-        public static final int authnum3=0x7f07000a;
-        public static final int btnAuthenticate=0x7f07000c;
-        public static final int btnHelp=0x7f07000b;
-        public static final int button_commit=0x7f070018;
-        public static final int checkbox_userComments=0x7f070017;
-        public static final int checkbox_userPhotos=0x7f070016;
-        public static final int connect_button=0x7f07000e;
-        public static final int connected_to_flickr=0x7f070015;
-        public static final int event_photo_icon=0x7f07000f;
-        public static final int event_photo_text=0x7f070010;
-        public static final int events=0x7f070013;
-        public static final int loading_text=0x7f070014;
-        public static final int main_layout=0x7f07000d;
-        public static final int photoBitmap=0x7f070011;
-        public static final int photoText=0x7f070012;
-        public static final int root=0x7f070000;
+        public static final int AuthHelpInfo=0x7f080001;
+        public static final int AuthInputLayout=0x7f080007;
+        public static final int AuthLayout=0x7f080003;
+        public static final int AuthWeb=0x7f080004;
+        public static final int BtnOK=0x7f080002;
+        public static final int LblAuthCode=0x7f080006;
+        public static final int TokenInputLayout=0x7f080005;
+        public static final int authnum1=0x7f080008;
+        public static final int authnum2=0x7f080009;
+        public static final int authnum3=0x7f08000a;
+        public static final int btnAuthenticate=0x7f08000c;
+        public static final int btnHelp=0x7f08000b;
+        public static final int button_commit=0x7f080018;
+        public static final int checkbox_userComments=0x7f080017;
+        public static final int checkbox_userPhotos=0x7f080016;
+        public static final int connect_button=0x7f08000e;
+        public static final int connected_to_flickr=0x7f080015;
+        public static final int event_photo_icon=0x7f08000f;
+        public static final int event_photo_text=0x7f080010;
+        public static final int events=0x7f080013;
+        public static final int loading_text=0x7f080014;
+        public static final int main_layout=0x7f08000d;
+        public static final int photoBitmap=0x7f080011;
+        public static final int photoText=0x7f080012;
+        public static final int root=0x7f080000;
     }
     public static final class layout {
         public static final int appwidget=0x7f030000;
@@ -56,57 +56,60 @@ public final class R {
         public static final int loading=0x7f030006;
         public static final int widget_configure=0x7f030007;
     }
+    public static final class raw {
+        public static final int authenticate_help=0x7f050000;
+    }
     public static final class string {
-        public static final int added_to_gallery=0x7f050020;
-        public static final int alert_disconnect=0x7f05001a;
-        public static final int api_key=0x7f05000e;
-        public static final int api_secret=0x7f05000f;
-        public static final int app_name=0x7f050000;
-        public static final int auth_code=0x7f050002;
-        public static final int auth_url=0x7f050010;
-        public static final int authenticate=0x7f050001;
-        public static final int btnauthenticate=0x7f050003;
-        public static final int btncancellabel=0x7f050004;
-        public static final int btndelete=0x7f050005;
-        public static final int btngetcode=0x7f050006;
-        public static final int btnhelp=0x7f050007;
-        public static final int btnok=0x7f050008;
-        public static final int cancel=0x7f05001e;
-        public static final int checking_network=0x7f050025;
-        public static final int comment=0x7f050022;
-        public static final int connectKO=0x7f050013;
-        public static final int connectOK=0x7f050012;
-        public static final int connected_to_flickr=0x7f050017;
-        public static final int content_userComments=0x7f050016;
-        public static final int content_userPhotos=0x7f050014;
-        public static final int disconnect=0x7f050018;
-        public static final int disconnect_ok=0x7f050019;
-        public static final int done=0x7f05001d;
-        public static final int error=0x7f050024;
-        public static final int fave=0x7f050021;
-        public static final int loading=0x7f05001f;
-        public static final int msgauthhelp=0x7f050009;
-        public static final int msgnnetworkerror=0x7f05000a;
-        public static final int no=0x7f05001c;
-        public static final int no_network=0x7f050023;
-        public static final int please_configure=0x7f050015;
-        public static final int ttlerror=0x7f05000b;
-        public static final int ttlhelp=0x7f05000d;
-        public static final int ttlwarning=0x7f05000c;
-        public static final int version=0x7f050011;
-        public static final int yes=0x7f05001b;
+        public static final int added_to_gallery=0x7f060020;
+        public static final int alert_disconnect=0x7f06001a;
+        public static final int api_key=0x7f06000e;
+        public static final int api_secret=0x7f06000f;
+        public static final int app_name=0x7f060000;
+        public static final int auth_code=0x7f060002;
+        public static final int auth_url=0x7f060010;
+        public static final int authenticate=0x7f060001;
+        public static final int btnauthenticate=0x7f060003;
+        public static final int btncancellabel=0x7f060004;
+        public static final int btndelete=0x7f060005;
+        public static final int btngetcode=0x7f060006;
+        public static final int btnhelp=0x7f060007;
+        public static final int btnok=0x7f060008;
+        public static final int cancel=0x7f06001e;
+        public static final int checking_network=0x7f060025;
+        public static final int comment=0x7f060022;
+        public static final int connectKO=0x7f060013;
+        public static final int connectOK=0x7f060012;
+        public static final int connected_to_flickr=0x7f060017;
+        public static final int content_userComments=0x7f060016;
+        public static final int content_userPhotos=0x7f060014;
+        public static final int disconnect=0x7f060018;
+        public static final int disconnect_ok=0x7f060019;
+        public static final int done=0x7f06001d;
+        public static final int error=0x7f060024;
+        public static final int fave=0x7f060021;
+        public static final int loading=0x7f06001f;
+        public static final int msgauthhelp=0x7f060009;
+        public static final int msgnnetworkerror=0x7f06000a;
+        public static final int no=0x7f06001c;
+        public static final int no_network=0x7f060023;
+        public static final int please_configure=0x7f060015;
+        public static final int ttlerror=0x7f06000b;
+        public static final int ttlhelp=0x7f06000d;
+        public static final int ttlwarning=0x7f06000c;
+        public static final int version=0x7f060011;
+        public static final int yes=0x7f06001b;
     }
     public static final class style {
-        public static final int FlickrWidget=0x7f060000;
-        public static final int FlickrWidget_EventIcon=0x7f060005;
-        public static final int FlickrWidget_LoadingText=0x7f060002;
-        public static final int FlickrWidget_Photo=0x7f060006;
-        public static final int FlickrWidget_Text=0x7f060003;
-        public static final int FlickrWidget_Text_Title=0x7f060004;
+        public static final int FlickrWidget=0x7f070000;
+        public static final int FlickrWidget_EventIcon=0x7f070005;
+        public static final int FlickrWidget_LoadingText=0x7f070002;
+        public static final int FlickrWidget_Photo=0x7f070006;
+        public static final int FlickrWidget_Text=0x7f070003;
+        public static final int FlickrWidget_Text_Title=0x7f070004;
         /**  Widget styles 
          */
-        public static final int FlickrWidget_Widget=0x7f060001;
-        public static final int Theme_FlickrWidget=0x7f060007;
+        public static final int FlickrWidget_Widget=0x7f070001;
+        public static final int Theme_FlickrWidget=0x7f070007;
     }
     public static final class xml {
         public static final int appwidget_info_4x1=0x7f040000;
